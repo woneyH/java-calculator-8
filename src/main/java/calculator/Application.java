246352a7e953cefb9be21e81d4cf3187f25e4015
김원혁 <kwh102455@gmail.com>
2025-10-19 12:30:16 +0900

    feat(Application): Application 클래스

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import controller.Controller;
+import domain.Calculator;
+import service.ParserService;
+import view.InputView;
+import view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = new Controller(
+                new InputView(),
+                new OutputView(),
+                new Calculator(),
+                new ParserService()
+        );
+        controller.run();
     }
 }
